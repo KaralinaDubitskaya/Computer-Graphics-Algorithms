daf91fb7f7f2798bfc49a517e5c370dd8186c513
Caroline <dubitskaya.karalina@gmail.com>
2019-11-25 00:20:05 +0300

    добавлена модель z-buffer

--- CGA/CGA/models/ZBuffer.cs
+++ CGA/CGA/models/ZBuffer.cs
@@ -8,5 +8,47 @@ namespace CGA.models
 {
     public class ZBuffer
     {
+        private double[,] _buffer;
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public ZBuffer(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            _buffer = new double[Width, Height];
+        }
+
+        private bool IsValidPixel(int x, int y)
+        {
+            return x >= 0 && x <= Width && y >= 0 && y <= Height;
+        }
+
+        public double this[int x, int y]
+        {
+            get
+            {
+                if (IsValidPixel(x, y))
+                {
+                    return _buffer[x, y];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            set
+            {
+                if (IsValidPixel(x, y))
+                {
+                    _buffer[x, y] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
     }
 }
