49e0dbe946cb70d146680ff699db2106109419ab
Diana Arabey <diana.arabey@mail.ru>
2019-11-24 15:30:07 +0300

    add parser

--- CGA/CGA/MainWindow.xaml.cs
+++ CGA/CGA/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CGA.models;
+using CGA.parser;
+using CGA.utils;
 
 namespace CGA
 {
@@ -20,9 +23,16 @@ namespace CGA
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Model model;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] fileLines = OpenReadFile.Execute();
+            model = ObjParser.parse(fileLines);
         }
     }
 }
